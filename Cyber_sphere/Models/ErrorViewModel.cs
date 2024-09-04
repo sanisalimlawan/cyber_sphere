217015bf163bfb702d034d1a5bf346309ebe9bca
sani salim lawan <sanisalim2005@gmail.com>
2024-09-03 19:32:16 -0700

    Add project files.

---
+++ Cyber_sphere/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cyber_sphere.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
